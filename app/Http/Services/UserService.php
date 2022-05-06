8461a37358b29ae4144c67b49d2b0ea140b3667a
Nguyen Thi Tra <lionelnguyen1998@gmail.com>
2022-05-06 09:09:01 +0700

    fixbug

--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -212,9 +212,9 @@ class UserService implements UserServiceInterface
     {
         $avatarDefault = "https://res.cloudinary.com/daqvhmyif/image/upload/v1650429693/wtatjbj7jhpueicdrg6n.jpg";
 
-        if (isset($request['avatar'])) {
-            $request['avatar'] = $this->uploadService->store($request['avatar']);
-        }
+        // if (isset($request['avatar'])) {
+        //     $request['avatar'] = $this->uploadService->store($request['avatar']);
+        // }
 
         $user = User::create([
             'name' => $request['name'],
@@ -240,9 +240,9 @@ class UserService implements UserServiceInterface
 
     public function edit($request)
     {
-        if (isset($request['avatar'])) {
-            $request['avatar'] = $this->uploadService->store($request['avatar']);
-        }
+        // if (isset($request['avatar'])) {
+        //     // $request['avatar'] = $this->uploadService->store($request['avatar']);
+        // }
 
         $request['password'] = Hash::make($request['password']);
 
