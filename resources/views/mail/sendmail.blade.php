b96372ffbc9a0a93c2296a535554ae4187e216b3
Nguyen Thi Tra <lionelnguyen1998@gmail.com>
2022-06-04 13:01:07 +0700

    fixbug

--- resources/views/mail/sendmail.blade.php
+++ resources/views/mail/sendmail.blade.php
@@ -3,27 +3,27 @@ $reportType  = config('const.report_type');
 $index = $contacts['report_type']
 @endphp
 <h4>
-   タイトル: {{ $reportType[$index] }}
+   タイトル/Chủ đề: {{ $reportType[$index] }}
 </h4>
 <h4>* * * * * * * * * * * *</h4>
 <h4>
-   差出人: {{ $contacts['name'] }}
+   差出人/Người gửi: {{ $contacts['name'] }}
 </h4>
 <h4>* * * * * * * * * * * *</h4>
 <h4>
-   メールアドレス: {{ $contacts['email'] }}
+   メールアドレス/Địa chỉ Email: {{ $contacts['email'] }}
 </h4>
 <h4>* * * * * * * * * * * *</h4>
 <h4>
-   電話番号: {{ $contacts['phone'] }}
+   電話番号/Số điện thoại: {{ $contacts['phone'] }}
 </h4>
 <h4>* * * * * * * * * * * *</h4>
 <h4>
-   写真:{{ $contacts["file"] }}
+   写真/Hình ảnh:{{ $contacts["file"] }}
 </h4>
 <h4>* * * * * * * * * * * *</h4>
 <div>
-    <h4>内容</h4>
+    <h4>内容/Nội dung</h4>
     <p>
         {{ $contacts['content'] }}
     </p>
